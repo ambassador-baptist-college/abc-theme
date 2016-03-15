a5469721fb1d4fa5df07fa6a0eb57186145785ee
Andrew Minion <andrew@andrewrminion.com>
2016-03-15 14:16:13 -0400

    initial theme setup
    Use custom minified CSS, custom web fonts

---
+++ Gruntfile.js
@@ -0,0 +1,69 @@
+module.exports = function (grunt) {
+  grunt.initConfig({
+    // Watch task config
+    watch: {
+        styles: {
+            files: "scss/*.scss",
+            tasks: ['sass', 'postcss'],
+        },
+        javascript: {
+            files: ["js/*.js", "!js/*.min.js"],
+            tasks: ['uglify'],
+        },
+    },
+    sass: {
+        dist: {
+            options: {
+                style: 'compressed'
+            },
+            files: {
+                "css/main.min.css" : "scss/main.scss",
+            }
+        }
+    },
+    postcss: {
+        options: {
+            map: {
+                inline: false,
+            },
+
+            processors: [
+                require('pixrem')(), // add fallbacks for rem units
+                require('autoprefixer')({browsers: 'last 2 versions'}), // add vendor prefixes
+                require('cssnano')() // minify the result
+            ]
+        },
+        dist: {
+            src: 'css/main.min.css',
+        }
+    },
+    uglify: {
+        custom: {
+            files: {
+//                'js/navigation.min.js': ['js/navigation.js'],
+            },
+        },
+    },
+    browserSync: {
+        dev: {
+            bsFiles: {
+                src : ['*.css', '**/*.php', '**/*.js', '!node_modules'],
+            },
+            options: {
+                watchTask: true,
+                proxy: "http://dev.abc.dev",
+            },
+        },
+    },
+  });
+
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-postcss');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-browser-sync');
+    grunt.registerTask('default', [
+        'browserSync',
+        'watch',
+    ]);
+};
---
+++ functions.php
@@ -0,0 +1,29 @@
+<?php
+
+// add minified CSS
+add_action( 'wp_enqueue_scripts', 'abc_minified_css', 5 );
+function abc_minified_css() {
+    // add minified stylesheet
+    wp_enqueue_style( 'abc-style', get_stylesheet_directory_uri() . '/css/main.min.css' );
+}
+
+// replace default fonts and stylesheet
+add_action( 'wp_enqueue_scripts', 'abc_webfonts_remove', 20 );
+function abc_webfonts_remove() {
+    wp_deregister_style( 'twentysixteen-style' );
+    wp_deregister_style( 'twentysixteen-fonts' );
+    wp_deregister_style( 'open-sans' );
+}
+add_action( 'wp_footer', 'abc_webfonts_add' );
+function abc_webfonts_add() {
+    ?><script>
+      (function(d) {
+        var config = {
+          kitId: 'umm0dso',
+          scriptTimeout: 3000,
+          async: true
+        },
+        h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+      })(document);
+    </script><?php
+}
