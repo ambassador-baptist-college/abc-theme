713938e25642bda6687052574edf532410ae29db
Andrew Minion <andrew@andrewrminion.com>
2016-07-25 21:26:58 -0400

    add homepage content

--- page-home.php
+++ page-home.php
@@ -55,6 +55,131 @@ get_header(); ?>
         endwhile;
         ?>
 
+        <section class="home-features home-stripe">
+            <section class="events">
+                <header>
+                    <h1 class="entry-title">Events</h1>
+                </header>
+                <?php
+                    if ( get_field( 'events_content' ) ) {
+                        the_field( 'events_content' );
+                    }
+                    if ( get_field( 'events_link' ) ) {
+                        echo '<p class="read-more"><a href="' . get_field( 'events_link' ) . '">Read more</a></p>';
+                    }
+                ?>
+            </section>
+            <section class="news">
+                <header>
+                    <h1 class="entry-title">News</h1>
+                </header>
+                <?php
+                    // WP_Query arguments
+                    $args = array (
+                        'post_type'              => array( 'post' ),
+                        'posts_per_page'         => '5',
+                        'post_status'            => 'publish',
+                    );
+
+                    // The Query
+                    $news_query = new WP_Query( $args );
+                    // The Loop
+                    if ( $news_query->have_posts() ) {
+                        echo '<ul>';
+                        while ( $news_query->have_posts() ) {
+                            $news_query->the_post();
+                            echo '<li><a href="' . get_permalink() . '">' . get_the_title() . '</a></li>';
+                        }
+                        echo '</ul>
+                        <p class="read-more"><a href="' . get_permalink( get_option( 'page_for_posts' ) ) . '">Read More</a></p>';
+                    }
+                    wp_reset_postdata();
+                ?>
+            </section>
+            <section class="info">
+                <header>
+                    <h1 class="entry-title">Info</h1>
+                </header>
+                <?php
+                    if ( get_field( 'contact_info_content' ) ) {
+                        the_field( 'contact_info_content' );
+                    }
+                    if ( get_field( 'contact_info_link' ) ) {
+                        echo '<p class="read-more"><a href="' . get_field( 'contact_info_link' ) . '">Read more</a></p>';
+                    }
+                ?>
+            </section>
+        </section><!-- .home-features -->
+
+        <section class="apply home-stripe">
+        <?php
+            if ( get_field( 'cta_headline' ) ) {
+                the_field( 'cta_headline' );
+            }
+            if ( get_field( 'cta_target' ) && get_field( 'cta_button_text' ) ) {
+                echo '<a class="button green" href="' . get_field( 'cta_target' ) . '">' . get_field( 'cta_button_text' ) . '</a>';
+            }
+        ?>
+        </section><!-- .apply -->
+
+        <section class="video home-stripe">
+        <?php
+            if ( get_field( 'video_content' ) ) {
+                the_field( 'video_content' );
+            }
+            if ( get_field( 'video_link' ) ) {
+                the_field( 'video_link' );
+            }
+        ?>
+        </section><!-- .video -->
+
+        <section class="latest-sermon home-stripe">
+        <?php
+            if ( get_field( 'latest_sermon' ) ) {
+                the_field( 'latest_sermon' );
+
+                // WP_Query arguments
+                $args = array (
+                    'post_type'              => array( 'wpfc_sermon' ),
+                    'posts_per_page'         => '1',
+                    'post_status'            => 'publish',
+                    'order'                  => 'DESC',
+                    'orderby'                => 'meta_value',
+                    'meta_query'             => array(
+                        array(
+                            'key'       => 'sermon_date',
+                            'type'      => 'NUMERIC',
+                        ),
+                    ),
+                    'no_found_rows'          => true,
+                );
+
+                // The Query
+                $sermons_query = new WP_Query( $args );
+                // The Loop
+                if ( $sermons_query->have_posts() ) {
+                    while ( $sermons_query->have_posts() && $counter == 0) {
+                        $sermons_query->the_post();
+
+                        echo '<h2><a id="latest_sermon_title" title="' . esc_attr( get_the_title() ) . '" href="' . get_permalink() . '">' . get_the_title() . '</a></h2>
+                        <p class="meta">';
+                            // preacher
+                            the_terms( $post->ID, 'wpfc_preacher', '<span class="preacher">', ', ', '</span> | ' );
+
+                            // sermon date
+                            wpfc_sermon_date( get_option( 'date_format' ) );
+
+                            // Bible passage
+                            wpfc_sermon_meta( 'bible_passage', ' | <span class="bible_passage">', '</span>' );
+                        echo '</p>';
+                        wpfc_sermon_files();
+                    }
+                }
+                // reset the query
+                wp_reset_postdata();
+            }
+        ?>
+        </section>
     </main><!-- .site-main -->
 
 </div><!-- .content-area -->
