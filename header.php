ea7bf8c58d12f2f542e6384bfe02e51fa231545a
Andrew Minion <andrew@andrewrminion.com>
2017-06-15 22:50:00 -0400

    add ABC logo to site-header and menu

--- header.php
+++ header.php
@@ -38,9 +38,13 @@
         <div class="site-header-main site-inner site-content">
             <div class="site-branding">
                 <?php if ( is_front_page() && is_home() ) : ?>
-                    <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+                    <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="Ambassador Baptist College" rel="home">
+                        <?php include( get_stylesheet_directory() . '/img/ABC-logo.svg' ); ?>
+                    </a></h1>
                 <?php else : ?>
-                    <p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+                    <p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="Ambassador Baptist College" rel="home">
+                        <?php include( get_stylesheet_directory() . '/img/ABC-logo.svg' ); ?>
+                    </a></p>
                 <?php endif; ?>
             </div><!-- .site-branding -->
 
