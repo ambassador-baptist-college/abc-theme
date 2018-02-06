f119209e59bb3d71894e211cde318d323289402b
Andrew Minion <andrew@andrewrminion.com>
2018-02-06 16:32:48 -0500

    add placeholder attributes for Camp and banquets

--- tribe-events/meta/text.php
+++ tribe-events/meta/text.php
@@ -11,6 +11,40 @@
  */
 $multiline = isset( $field['extra'] ) && isset( $field['extra']['multiline'] ) ? $field['extra']['multiline'] : '';
 $option_id = "tribe-tickets-meta_{$this->slug}" . ( $attendee_id ? '_' . $attendee_id : '' );
+
+switch ( $field['label'] ) {
+    // Camp Barnabas
+    case 'Name':
+        $placeholder = 'John Doe';
+        break;
+    case 'Age':
+        $placeholder = '14';
+        break;
+    case 'Camper’s Physician':
+        $placeholder = 'John Doe';
+        break;
+    case 'Physician’s Phone':
+        $placeholder = '234-567-8901';
+        break;
+    case 'Date of Birth':
+        $placeholder = '1/1/' . ( (int) date( 'Y' ) - 14 );
+        break;
+    case 'Emergency Contact':
+        $placeholder = "Jane Doe: 234-567-8901\nJim Doe: 234-567-8901";
+        break;
+
+    // banquets
+    case 'First Name':
+        $placeholder = 'John';
+        break;
+    case 'Last Name':
+        $placeholder = 'Doe';
+        break;
+    case 'Box Number (students only)':
+        $placeholder = '4000';
+        break;
+}
+
 ?>
 <div class="tribe-tickets-meta tribe-tickets-meta-text <?php echo $required ? 'tribe-tickets-meta-required' : ''; ?>">
     <label for="<?php echo esc_attr( $option_id ); ?>"><?php echo wp_kses_post( $field['label'] ); ?></label>
@@ -21,6 +55,7 @@ $option_id = "tribe-tickets-meta_{$this->slug}" . ( $attendee_id ? '_' . $attend
             id="<?php echo esc_attr( $option_id ); ?>"
             class="ticket-meta"
             name="tribe-tickets-meta[<?php echo $attendee_id ?>][<?php echo esc_attr( $this->slug ); ?>]"
+            placeholder="<?php echo $placeholder; ?>"
             <?php echo $required ? 'required' : ''; ?>
             <?php disabled( $this->is_restricted( $attendee_id ) ); ?>
         ><?php echo esc_textarea( $value ); ?></textarea>
@@ -32,6 +67,7 @@ $option_id = "tribe-tickets-meta_{$this->slug}" . ( $attendee_id ? '_' . $attend
             id="<?php echo esc_attr( $option_id ); ?>"
             class="ticket-meta"
             name="tribe-tickets-meta[<?php echo $attendee_id ?>][<?php echo esc_attr( $this->slug ); ?>]"
+            placeholder="<?php echo $placeholder; ?>"
             value="<?php echo esc_attr( $value ); ?>"
             <?php echo $required ? 'required' : ''; ?>
             <?php disabled( $this->is_restricted( $attendee_id ) ); ?>
