7fa65039c207c3d5722b8c4c9120e31c9fe37ba1
Andrew Minion <andrew@andrewrminion.com>
2018-05-18 09:04:54 -0400

    fix whitespace issues

--- footer.php
+++ footer.php
@@ -22,11 +22,11 @@
 				<nav class="main-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Footer Primary Menu', 'twentysixteen' ); ?>">
 					<?php
 						wp_nav_menu(
-							 array(
-								 'theme_location' => 'primary',
-								 'menu_class'     => 'primary-menu',
-							 )
-							);
+							array(
+								'theme_location' => 'primary',
+								'menu_class'     => 'primary-menu',
+							)
+						);
 					?>
 				</nav><!-- .main-navigation -->
 			<?php endif; ?>
@@ -35,14 +35,14 @@
 				<nav class="social-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Footer Social Links Menu', 'twentysixteen' ); ?>">
 					<?php
 						wp_nav_menu(
-							 array(
-								 'theme_location' => 'social',
-								 'menu_class'     => 'social-links-menu',
-								 'depth'          => 1,
-								 'link_before'    => '<span class="screen-reader-text">',
-								 'link_after'     => '</span>',
-							 )
-							);
+							array(
+								'theme_location' => 'social',
+								'menu_class'     => 'social-links-menu',
+								'depth'          => 1,
+								'link_before'    => '<span class="screen-reader-text">',
+								'link_after'     => '</span>',
+							)
+						);
 					?>
 				</nav><!-- .social-navigation -->
 			<?php endif; ?>
