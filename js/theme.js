1bdd81d9720a73ad290b711fe6f5acf2150221c4
Andrew Minion <andrew@andrewrminion.com>
2018-08-02 16:15:01 -0400

    update JS code style

--- js/theme.js
+++ js/theme.js
@@ -1,76 +1,88 @@
-(function($){
-    $(document).ready(function(){
+/**
+ * ABC theme functions.
+ */
 
-        // live streaming banner
-        if ($('.wp-youtube-live').hasClass('live')) {
-            $('.streaming.off-air').removeClass('off-air').addClass('on-air');
-        }
-        window.addEventListener('wpYouTubeLiveStarted', function() {
-            if ($('.wp-youtube-live').hasClass('live')) {
-                $('.streaming.off-air').removeClass('off-air').addClass('on-air');
-            }
-        });
+/* global jQuery */
+'use strict';
+(function($) {
+
+	/**
+	 * Show/hide transcript delivery method fields.
+	 *
+	 * @returns {void} Modifies DOM.
+	 */
+	function updateDeliveryMethod() {
+		if ($('select[name="delivery-type"]').length > 0) {
+			var deliveryMethod = $('select[name="delivery-type"]').val().toLowerCase().replace(' ', '-');
+
+			$('.delivery').hide();
+			$('.' + deliveryMethod).show();
+		}
+	}
+
+	$(document).ready(function() {
+
+		// live streaming banner
+		if ($('.wp-youtube-live').hasClass('live')) {
+			$('.streaming.off-air').removeClass('off-air').addClass('on-air');
+		}
+		window.addEventListener('wpYouTubeLiveStarted', function() {
+			if ($('.wp-youtube-live').hasClass('live')) {
+				$('.streaming.off-air').removeClass('off-air').addClass('on-air');
+			}
+		});
 
 
-        // live streaming popup
-        $('.streaming-frame, .streaming-frame-shade').hide();
-        $('#streaming-popup').on('click', function(e) {
-            e.preventDefault();
-            $('.streaming-frame, .streaming-frame-shade').toggle();
-        });
-        $('.streaming-frame .close, .streaming-frame-shade').on('click', function(e) {
-            $('.streaming-frame, .streaming-frame-shade').hide();
-        })
+		// live streaming popup
+		$('.streaming-frame, .streaming-frame-shade').hide();
+		$('#streaming-popup').on('click', function(e) {
+			e.preventDefault();
+			$('.streaming-frame, .streaming-frame-shade').toggle();
+		});
+		$('.streaming-frame .close, .streaming-frame-shade').on('click', function(e) {
+			e.preventDefault();
+			$('.streaming-frame, .streaming-frame-shade').hide();
+		});
 
-        // chosen
-        $('select.sermon-search, select[name="archive-dropdown"]').chosen();
+		// chosen
+		$('select.sermon-search, select[name="archive-dropdown"]').chosen();
 
-        // hide collapse-form and add toggle button
-        if ($('.collapse-form').hasClass('payment')) {
-            var buttonText = 'Pay Now';
-        } else {
-            var buttonText = 'Give Now';
-        }
-        $('.collapse-form').slideUp().before('<p><button type="button" class="toggle-form">' + buttonText + ' <span class="dashicons dashicons-arrow-down-alt2"></span></button></p>');
+		// hide collapse-form and add toggle button
+		var buttonText;
 
-        // handle collapse-form toggle buttons
-        $(document).on('click', 'button.toggle-form', function(e){
-            e.preventDefault();
+		if ($('.collapse-form').hasClass('payment')) {
+			buttonText = 'Pay Now';
+		} else {
+			buttonText = 'Give Now';
+		}
+		$('.collapse-form').slideUp().before('<p><button type="button" class="toggle-form">' + buttonText + ' <span class="dashicons dashicons-arrow-down-alt2"></span></button></p>');
 
-            // expand form
-            $(this).parent().next('.collapse-form').slideToggle();
+		// handle collapse-form toggle buttons
+		$(document).on('click', 'button.toggle-form', function(e) {
+			e.preventDefault();
 
-            // handle button visual state
-            $(this).find('span.dashicons').toggleClass('dashicons-arrow-up-alt2 dashicons-arrow-down-alt2')
-        });
+			// expand form
+			$(this).parent().next('.collapse-form').slideToggle();
 
-        // handle preset and custom donation/payment amounts
-        $('.wpcf7-form input[name="amount-other"]').slideUp();
-        $('.wpcf7-form select[name="preset-amount"]').on('change', function(){
-            var presetAmount = $(this).val(),
-                otherField = $(this).parents('p').find('input[name="amount-other"]');
+			// handle button visual state
+			$(this).find('span.dashicons').toggleClass('dashicons-arrow-up-alt2 dashicons-arrow-down-alt2');
+		});
 
-            if (presetAmount == 'Other') {
-                otherField.slideDown().focus();
-            } else {
-                otherField.slideUp();
-            }
-        });
+		// handle preset and custom donation/payment amounts
+		$('.wpcf7-form input[name="amount-other"]').slideUp();
+		$('.wpcf7-form select[name="preset-amount"]').on('change', function() {
+			var presetAmount = $(this).val(),
+				otherField = $(this).parents('p').find('input[name="amount-other"]');
 
-        // handle transcript delivery method
-        $('select[name="delivery-type"]').on('change', updateDeliveryMethod);
-        updateDeliveryMethod();
+			if ('Other' === presetAmount) {
+				otherField.slideDown().focus();
+			} else {
+				otherField.slideUp();
+			}
+		});
 
-        /**
-         * Show/hide transcript delivery method fields
-         */
-        function updateDeliveryMethod() {
-            if ($('select[name="delivery-type"]').length > 0) {
-                var deliveryMethod = $('select[name="delivery-type"]').val().toLowerCase().replace(' ', '-');
-
-                $('.delivery').hide();
-                $('.'+deliveryMethod).show();
-            }
-        }
-    });
-})(jQuery);
+		// handle transcript delivery method
+		$('select[name="delivery-type"]').on('change', updateDeliveryMethod);
+		updateDeliveryMethod();
+	});
+}(jQuery));
