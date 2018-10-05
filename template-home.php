df1d4d9301466597331b5389f9f64957ef98fb78
Andrew Minion <andrew@andrewrminion.com>
2018-10-05 09:17:58 -0400

    update PHPCS sniffs and fix some issues

--- template-home.php
+++ template-home.php
@@ -18,8 +18,8 @@ get_header(); ?>
 		<?php
 		// Start the loop.
 		while ( have_posts() ) :
-the_post();
-		?>
+			the_post();
+			?>
 
 			<?php
 			// Include the page content template.
@@ -31,15 +31,15 @@ the_post();
 					the_content();
 
 					wp_link_pages(
-						 array(
-							 'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentysixteen' ) . '</span>',
-							 'after'       => '</div>',
-							 'link_before' => '<span>',
-							 'link_after'  => '</span>',
-							 'pagelink'    => '<span class="screen-reader-text">' . __( 'Page', 'twentysixteen' ) . ' </span>%',
-							 'separator'   => '<span class="screen-reader-text">, </span>',
-						 )
-						);
+						array(
+							'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentysixteen' ) . '</span>',
+							'after'       => '</div>',
+							'link_before' => '<span>',
+							'link_after'  => '</span>',
+							'pagelink'    => '<span class="screen-reader-text">' . __( 'Page', 'twentysixteen' ) . ' </span>%',
+							'separator'   => '<span class="screen-reader-text">, </span>',
+						)
+					);
 					?>
 				</div><!-- .entry-content -->
 
@@ -64,124 +64,126 @@ the_post();
 
 		<?php if ( function_exists( 'get_field' ) ) : ?>
 
-		<?php if ( get_field( 'show_cancellation_notice' ) ) { ?>
-			<section class="cancellation home-stripe">
-			<?php
-				if ( get_field( 'live_streaming' ) ) {
-				echo '<div class="teaser">' . get_field( 'cancellation_notice' ) . '</div>'; // WPCS: XSS ok.
-				}
-			?>
-			</section><!-- .cancellation -->
-		<?php } ?>
-
-		<section class="streaming home-stripe off-air">
-		<?php
-			if ( get_field( 'live_streaming' ) ) {
-			echo '<div class="teaser">' . get_field( 'live_streaming' ) . '</div>
-				<div class="streaming-frame-shade"></div>
-				<div class="streaming-frame"><span class="close">&times;</span>' . do_shortcode( '[youtube_live autoplay="false" js_only="true"]' ) . '</div>'; // WPCS: XSS ok.
-			}
-		?>
-		</section><!-- .video.streaming -->
-
-		<section class="home-features home-stripe">
-			<?php $home_feature_styles = ''; ?>
-
-			<?php
-			if ( get_field( 'button_2_text' ) && get_field( 'button_2_link' ) && get_field( 'button_2_image' ) ) {
-				$home_feature_styles .= '.button-2 {background-image: url("' . esc_url( get_field( 'button_2_image' ) ) . '");}';
-				?>
-			<section class="button-2 events">
-				<?php echo do_shortcode( '[calendar id="' . get_field( 'button_2_calendar' ) . '"]' ); ?>
-				<h2 class="read-more"><a href="<?php the_field( 'button_2_link' ); ?>"><?php the_field( 'button_2_text' ); ?></a></h2>
-			</section>
-			<?php } ?>
-
-			<?php
-			if ( get_field( 'button_1_text' ) && get_field( 'button_1_link' ) && get_field( 'button_1_image' ) ) {
-				$home_feature_styles .= '.button-1 {background-image: url("' . esc_url( get_field( 'button_1_image' ) ) . '");}';
-				?>
-			<section class="button-1 students">
-				<h2 class="read-more"><a href="<?php the_field( 'button_1_link' ); ?>"><?php the_field( 'button_1_text' ); ?></a></h2>
-			</section>
-			<?php } ?>
-
-			<?php
-			if ( get_field( 'button_3_text' ) && get_field( 'button_3_link' ) && get_field( 'button_3_image' ) ) {
-				$home_feature_styles .= '.button-3 {background-image: url("' . esc_url( get_field( 'button_3_image' ) ) . '");}';
-				?>
-			<section class="button-3 news">
+			<?php if ( get_field( 'show_cancellation_notice' ) ) { ?>
+				<section class="cancellation home-stripe">
 				<?php
-				$args = array(
-					'post_type'              => array( 'post' ),
-					'posts_per_page'         => '3',
-					'post_status'            => 'publish',
-				);
-
-				$news_query = new WP_Query( $args );
-
-				if ( $news_query->have_posts() ) {
-					while ( $news_query->have_posts() ) {
-						$news_query->the_post();
-
-						add_filter( 'excerpt_length', 'abc_return_news_excerpt_length' );
-
-						echo '<article class="home-news-excerpt">';
-							echo '<h3 class="entry-title"><a href="' . esc_url( get_permalink() ) . '">' . esc_attr( get_the_title() ) . '</a></h3>';
-							the_excerpt();
-						echo '</article>' . "\n";
+					if ( get_field( 'live_streaming' ) ) {
+					echo '<div class="teaser">' . get_field( 'cancellation_notice' ) . '</div>'; // WPCS: XSS ok.
 					}
-				}
-				wp_reset_postdata();
 				?>
-				<h2 class="read-more"><a href="<?php the_field( 'button_3_link' ); ?>"><?php the_field( 'button_3_text' ); ?></a></h2>
-			</section>
-			<?php
-			}
-			if ( $home_feature_styles ) {
-			?>
-				<style type="text/css">
-					<?php echo $home_feature_styles; // WPCS: XSS ok. ?>
-				</style>
+				</section><!-- .cancellation -->
 			<?php } ?>
-		</section><!-- .home-features -->
 
-		<section class="apply home-stripe">
-		<?php
+			<section class="streaming home-stripe off-air">
+			<?php
+			if ( get_field( 'live_streaming' ) ) {
+				echo '<div class="teaser">' . get_field( 'live_streaming' ) . '</div>
+					<div class="streaming-frame-shade"></div>
+					<div class="streaming-frame"><span class="close">&times;</span>' . do_shortcode( '[youtube_live autoplay="false" js_only="true"]' ) . '</div>'; // WPCS: XSS ok.
+			}
+			?>
+			</section><!-- .streaming -->
+
+			<section class="home-features home-stripe">
+				<?php $home_feature_styles = ''; ?>
+
+				<?php
+				if ( get_field( 'button_2_text' ) && get_field( 'button_2_link' ) && get_field( 'button_2_image' ) ) {
+					$home_feature_styles .= '.button-2 {background-image: url("' . esc_url( get_field( 'button_2_image' ) ) . '");}';
+					?>
+				<section class="button-2 events">
+					<?php echo do_shortcode( '[calendar id="' . get_field( 'button_2_calendar' ) . '"]' ); ?>
+					<h2 class="read-more"><a href="<?php the_field( 'button_2_link' ); ?>"><?php the_field( 'button_2_text' ); ?></a></h2>
+				</section>
+				<?php } ?>
+
+				<?php
+				if ( get_field( 'button_1_text' ) && get_field( 'button_1_link' ) && get_field( 'button_1_image' ) ) {
+					$home_feature_styles .= '.button-1 {background-image: url("' . esc_url( get_field( 'button_1_image' ) ) . '");}';
+					?>
+				<section class="button-1 students">
+					<h2 class="read-more"><a href="<?php the_field( 'button_1_link' ); ?>"><?php the_field( 'button_1_text' ); ?></a></h2>
+				</section>
+				<?php } ?>
+
+				<?php
+				if ( get_field( 'button_3_text' ) && get_field( 'button_3_link' ) && get_field( 'button_3_image' ) ) {
+					$home_feature_styles .= '.button-3 {background-image: url("' . esc_url( get_field( 'button_3_image' ) ) . '");}';
+					?>
+					<section class="button-3 news">
+						<?php
+						$args = array(
+							'post_type'              => array( 'post' ),
+							'posts_per_page'         => '3',
+							'post_status'            => 'publish',
+						);
+
+						$news_query = new WP_Query( $args );
+
+						if ( $news_query->have_posts() ) {
+							while ( $news_query->have_posts() ) {
+								$news_query->the_post();
+
+								add_filter( 'excerpt_length', 'abc_return_news_excerpt_length' );
+
+								echo '<article class="home-news-excerpt">';
+									echo '<h3 class="entry-title"><a href="' . esc_url( get_permalink() ) . '">' . esc_attr( get_the_title() ) . '</a></h3>';
+									the_excerpt();
+								echo '</article>' . "\n";
+							}
+						}
+						wp_reset_postdata();
+						?>
+						<h2 class="read-more"><a href="<?php the_field( 'button_3_link' ); ?>"><?php the_field( 'button_3_text' ); ?></a></h2>
+					</section>
+					<?php
+				}
+				if ( $home_feature_styles ) {
+				?>
+					<style type="text/css">
+						<?php
+						echo $home_feature_styles; // WPCS: XSS ok.
+						?>
+					</style>
+				<?php } ?>
+			</section><!-- .home-features -->
+
+			<section class="apply home-stripe">
+			<?php
 			if ( get_field( 'cta_headline' ) ) {
 				the_field( 'cta_headline' );
 			}
 			if ( get_field( 'cta_target' ) && get_field( 'cta_button_text' ) ) {
 				echo '<a class="button green" href="' . get_field( 'cta_target' ) . '">' . get_field( 'cta_button_text' ) . '</a>'; // WPCS: XSS ok.
 			}
-		?>
-		</section><!-- .apply -->
+			?>
+			</section><!-- .apply -->
 
-		<?php
-		$all_events = get_field( 'highlighted_events' );
-		if ( isset( $all_events ) ) {
-			foreach ( $all_events as $event ) {
+			<?php
+			$all_events = get_field( 'highlighted_events' );
+			if ( isset( $all_events ) ) {
+				foreach ( $all_events as $event ) {
 
-				$event_id = $event['event']['id'];
-				$event_link = get_permalink( $event_id );
-				$event_excerpt = get_post_field( 'post_excerpt', $event_id );
+					$event_id = $event['event']['id'];
+					$event_link = get_permalink( $event_id );
+					$event_excerpt = get_post_field( 'post_excerpt', $event_id );
 
-				if ( $event['event']['use_manual_excerpt'] ) {
-					$excerpt = $event['event']['manual_excerpt'];
-				} elseif ( $event_excerpt ) {
-					$excerpt = apply_filters( 'the_excerpt', $event_excerpt );
-				} else {
-					$excerpt = strip_shortcodes( get_post_field( 'post_content', $event_id ) );
-					$excerpt = wp_trim_words( $excerpt, 25, '&hellip;' );
+					if ( $event['event']['use_manual_excerpt'] ) {
+						$excerpt = $event['event']['manual_excerpt'];
+					} elseif ( $event_excerpt ) {
+						$excerpt = apply_filters( 'the_excerpt', $event_excerpt );
+					} else {
+						$excerpt = strip_shortcodes( get_post_field( 'post_content', $event_id ) );
+						$excerpt = wp_trim_words( $excerpt, 25, '&hellip;' );
+					}
+
+					include 'inc/home-event-stripe-content.php';
 				}
-
-				include 'inc/home-event-stripe-content.php';
 			}
-		}
-		?>
+			?>
 
-		<section class="video home-stripe full-width">
-			<div class="container">
+			<section class="video home-stripe full-width">
+				<div class="container">
 			<?php
 				if ( get_field( 'video_content' ) ) {
 					echo '<div class="video-content">' . get_field( 'video_content' ) . '</div>'; // WPCS: XSS ok.
@@ -189,12 +191,12 @@ the_post();
 				if ( get_field( 'video_link' ) ) {
 					echo '<div class="video-link">' . get_field( 'video_link' ) . '</div>'; // WPCS: XSS ok.
 				}
-			?>
-			</div>
-		</section><!-- .video -->
+				?>
+				</div>
+			</section><!-- .video -->
 
-		<section class="latest-sermon home-stripe">
-		<?php
+			<section class="latest-sermon home-stripe">
+			<?php
 			if ( get_field( 'latest_sermon' ) ) {
 				the_field( 'latest_sermon' );
 
@@ -229,8 +231,8 @@ the_post();
 				// Reset the query.
 				wp_reset_postdata();
 			}
-		?>
-		</section>
+			?>
+			</section>
 		<?php endif; ?>
 	</main><!-- .site-main -->
 
